825566bc0d8616c87f3fc3332e3385357f5ff8ab
Goutomi Chatterjee <goutomi.chatterjee@aveva.com>
2020-06-01 20:36:36 +0530

    NameNumericCalc

--- app.js
+++ app.js
@@ -3,7 +3,21 @@
     const newLocal = 'myFirstcontroller';
     angular.module('myFirstAngularApp', [])
 
-    .controller(newLocal, function() {
+    .controller(newLocal, function($scope) {
+        $scope.name = "";
+        $scope.totalValue = 0;
 
+        $scope.displayNumericValue = function() {
+            var numericTextValue = calculateNoOfCharsInName($scope.name);
+            $scope.totalValue = numericTextValue;
+        };
+
+        function calculateNoOfCharsInName(string) {
+            var totalStringValue = 0;
+            for (let index = 0; index < string.length; index++) {
+                totalStringValue += string.charCodeAt(index);
+            }
+            return totalStringValue;
+        }
     })
 })();
